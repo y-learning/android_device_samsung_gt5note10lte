11e2b7b4e55128affb7a39fe1349f8b401933ce1
Virsys Electron <virsys.electron@gmail.com>
2018-09-11 21:05:50 +0200

    Init gt510wifi

---
+++ BoardConfig.mk
@@ -0,0 +1,94 @@
+#
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := msm8916
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
+
+# DTB
+TARGET_PREBUILT_DTB := device/samsung/gt510wifi/prebuilt/dt.img
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := gt510wifi,gt510wifixx,SM-T550,SM-P550
+
+# Encryption
+#TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TW_INCLUDE_CRYPTO := true
+
+# Debug
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# Kernel
+TARGET_KERNEL_ARCH := arm
+TARGET_PREBUILT_KERNEL := device/samsung/gt510wifi/prebuilt/kernel
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x1e00000 --dt device/samsung/gt510wifi/prebuilt/dt.img
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# MKBootImg
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gt510wifi/mkbootimg.mk
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3294625792
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608
+BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11900268544
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# TWRP
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 180
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_EXCLUDE_SUPERSU := true
+TARGET_RECOVERY_FSTAB := device/samsung/gt510wifi/recovery/root/etc/twrp.fstab
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS:= true
+
+# Oreo
+#TW_NO_LEGACY_PROPS := true
---
+++ README.md
@@ -0,0 +1,34 @@
+# TWRP Device configuration for Samsung Galaxy Tab A 9.7" WiFi (gt510wifi)
+
+Copyright 2018 - The OmniROM Project
+
+For building TWRP for Samsung Galaxy Tab A 9.7" WiFi ONLY.
+
+### Kernel Source
+Check here: https://github.com/Galaxy-MSM8916/android_kernel_samsung_msm8916
+
+### How to compile
+
+```sh
+. build/envsetup.sh
+lunch omni_gt510wifi-eng
+make -j64 recoveryimage
+```
+
+### Device specifications
+=====================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Qualcomm MSM8916 Quad-core 1.2 GHz ARM® Cortex™ A53
+CHIPSET | Qualcomm MSM8916 Snapdragon 410
+GPU     | Adreno 306
+Memory  | 1.5 GB
+Shipped Android Version | 5.1.1
+Storage | 16 GB
+MicroSD | Up to 256 GB
+Battery | 6000 mAh
+Display | 1024 x 768, 9.7" (246.38mm)
+Rear Camera  | 5.0 MP
+Front Camera | 2.0 MP
+
